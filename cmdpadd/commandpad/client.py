b1017094b20c388212b8dc9d8f9038d23db11d05
James Bliss <james.bliss@astro73.com>
2007-06-11 00:10:25 +0000

    * Seriously brought multi-device support to commandpad.client * Added signals for adding and removing devices * Made a release
    git-svn-id: file:///home/astronouth7303/www/astronouth7303/public_html/astro73.com/svnroot/commandpad/trunk@9 536712d7-3b65-4200-8bf4-cfdaa7821495

--- cmdpadd/commandpad/client.py
+++ cmdpadd/commandpad/client.py
@@ -16,13 +16,13 @@
 """
 Provides some utilities for clients of the command pad service.
 
-Note that for signals to be received, a mainloop must be ran. 
-CommandPadListener has methods to deal with this. In addition, threading is
-used. gobject and dbus.main.glib are init'd in threads by this package. Make
-sure that you do not violate this.
+Note that for signals to be received, a mainloop must be ran. There are 
+functions to deal with this. In addition, threading is used. gobject and 
+dbus.main.glib are init'd in threads by this package. Make sure that you do not
+violate this.
 
 The dbus.mainloop.glib.DBusGMainLoop is set as default. If you wish to change 
-this, do so before instantiating any CommandPadListener, or pass it as the 
+this, do so before instantiating any CommandPadListener or , or pass it as the 
 "mainloop" keyword argument to it.
 """
 from __future__ import absolute_import
@@ -39,7 +39,127 @@ from commandpad.device import BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BUTTON_5, 
 __all__ = 'CommandPadListener', 'BUTTON_1', 'BUTTON_2', \
           'BUTTON_3', 'BUTTON_4', 'BUTTON_5', 'BUTTON_6', 'BUTTON_7', \
           'BUTTON_8', 'BUTTON_9', 'BUTTON_A', 'BUTTON_B', 'buttonName', \
-          'modeName', 'MODE_A', 'MODE_B'
+          'modeName', 'MODE_A', 'MODE_B', 'startMainLoop', 'joinMainLoop', \
+          'quitMainLoop', 'runMainLoop', 'CPManagerListener'
+
+## Class attribute
+MainLoop = gobject.MainLoop()
+
+def startMainLoop(cls):
+	"""
+	Runs the DBus main loop in a new thread.
+	"""
+	global _thread, MainLoop
+	_thread = threading.Thread(target=MainLoop.run, name="dbus-mainloop")
+	_thread.start()
+	
+def joinMainLoop(cls, *p, **kw):
+	"""
+	Calls the join() method on the main loop thread created by startMainLoop()
+	"""
+	global _thread
+	_thread.join(*p, **kw)
+
+def quitMainLoop(cls):
+	"""
+	Tells the main loop thread started by startMainLoop() to stop.
+	"""
+	global MainLoop
+	MainLoop.quit()
+
+def runMainLoop(cls):
+	"""
+	Just runs the main loop instead of threading it.
+	"""
+	global MainLoop
+	try:
+		MainLoop.run()
+	except KeyboardInterrupt:
+		pass
+
+class CPManagerListener(object):
+	"""
+	Interfaces with DBus and CommandPadD.
+	
+	The Add and Remove callbacks have the same signature:
+	   def event(manager, object)
+	* manager - The manager that's doing the calling
+	* object - The device that was added/removed (a CommandPadListener)
+	"""
+	__add = None
+	__remove = None
+	Add = property((lambda s: s.__add), (lambda s,v: setattr(s, '_CPManagerListener__add',v)), doc="""The callback for addDevice events.""")
+	Remove = property((lambda s: s.__remove), (lambda s,v: setattr(s, '_CPManagerListener__remove',v)), doc="""The callback for removeDevice events.""")
+	__bus = None
+	__receiver = None
+	__listeners = None
+	__mainloop = None
+	__object = None
+	def __init__(self, add=None, remove=None, mainloop=None):
+		"""CPManagerListener(mainloop=None) -> CommandPadListener
+		Creates a CommandPadListener.
+		* mainloop - the mainloop object to pass to dbus.SystemBus()
+		Note that as soon as Remove() returns, the CommandPadListener is 
+		destroyed.
+		"""
+		self.__listeners = {}
+		self.__mainloop = mainloop
+		if mainloop is None:
+			self.__bus = dbus.SystemBus()
+		else:
+			self.__bus = dbus.SystemBus(mainloop=mainloop)
+		self.__receiver = self.__bus.add_signal_receiver(self.__signal, 
+		       None, None, 'com.astro73.saitek.CPManager', None,
+		       member_keyword='member', path_keyword='path')
+		self.__object = self.__bus.get_object('com.astro73.saitek.CommandPad', "/com/astro73/saitek/CPManager")
+	
+	def close(self):
+		if self.__listeners is not None:
+			self.__receiver.remove()
+		self.__receiver = None
+		self.__add = None
+		self.__remove = None
+		self.__bus = None
+		self.__device = None
+		self.__filter = None
+		self.__mainloop = None
+		self.__object = None
+		if self.__listeners is not None:
+			for v in self.__listeners.values():
+				v.close()
+		self.__listeners = None
+		
+	def __del__(self):
+		self.close()
+	
+	def __signal(self, dev, member, path):
+		if member == 'addDevice' and callable(self.Add):
+			if dev not in self.__listeners:
+				self.__listeners[dev] = CommandPadListener(device=dev, mainloop=self.__mainloop)
+			self.Add(self, self.__listeners[dev])
+		elif member == 'removeDevice' and callable(self.Remove):
+			l = self.__listeners.pop(dev)
+			self.Remove(self, l)
+			l.close()
+	
+	def Devices(self):
+		"""
+		Returns a list of listeners for all the devices the daemon recognizes. 
+		Also synchronizes our list of devices with the daemon's.
+		"""
+		devs = self.__object.Devices()
+		rv = []
+		for d in self.__listeners.keys():
+			if d not in devs:
+				l = self.__listeners.pop(dev)
+				self.Remove(self, l)
+				l.close()
+		for dev in devs:
+			if dev not in self.__listeners:
+				self.__listeners[dev] = CommandPadListener(device=dev, mainloop=self.__mainloop)
+				self.Add(self, self.__listeners[dev])
+			rv.append(self.__listeners[dev])
+		return rv
 
 class CommandPadListener(object):
 	"""
@@ -56,9 +176,6 @@ class CommandPadListener(object):
 	* oldmode - The mode set before the button was pushed
 	* newmode - The mode now set
 	"""
-	## Class attribute
-	MainLoop = gobject.MainLoop()
-	
 	__up = None
 	__down = None
 	__mode = None
@@ -67,16 +184,19 @@ class CommandPadListener(object):
 	ButtonUp = property((lambda s: s.__up), (lambda s,v: setattr(s, '_CommandPadListener__up',v)), doc="""The callback for ButtonUp events.""")
 	ButtonDown = property((lambda s: s.__down), (lambda s,v: setattr(s, '_CommandPadListener__down',v)), doc="""The callback for ButtonUp events.""")
 	ModeChange = property((lambda s: s.__mode), (lambda s,v: setattr(s, '_CommandPadListener__mode',v)), doc="""The callback for ButtonUp events.""")
+	Device = property((lambda s: s.__device), doc="""The specific device to listen to.""")
 	Filter = property((lambda s: s.__filter), (lambda s,v: setattr(s, '_CommandPadListener__filter',v)), doc="""Do we filter out the mode buttons?""")
 	__bus = None
-	__thread = None
 	__receiver = None
-	def __init__(self, up=None, down=None, mode=None, mainloop=None):
-		"""CommandPadListener(up=None, down=None, mode=None, mainloop=None) -> CommandPadListener
+	__device = None
+	self.__object = None
+	def __init__(self, up=None, down=None, mode=None, device=None, mainloop=None):
+		"""CommandPadListener(up=None, down=None, mode=None, device=None, mainloop=None) -> CommandPadListener
 		Creates a CommandPadListener.
 		* up - the ButtonUp handler or None
 		* down - the ButtonDown handler or None
 		* mode - the ModeChange handler or None
+		* device - the specific device to listen to or None
 		* mainloop - the mainloop object to pass to dbus.SystemBus()
 		"""
 		if up is not None:
@@ -85,22 +205,35 @@ class CommandPadListener(object):
 			self.ButtonDown = down
 		if mode is not None:
 			self.ModeChange = mode
-		print "Creating Bus"
+		self.__device = device
 		if mainloop is None:
 			self.__bus = dbus.SystemBus()
 		else:
 			self.__bus = dbus.SystemBus(mainloop=mainloop)
-		print "\tbus:", repr(self.__bus)
-		print "Adding signal handler"
 		self.__receiver = self.__bus.add_signal_receiver(self.__signal, 
-		       None, None, 'com.astro73.saitek.CommandPad', None,
+		       None, None, 'com.astro73.saitek.CommandPad', self.__device,
 		       member_keyword='member', path_keyword='path')
-		print "\tval:", repr(self.__receiver)
+		if self.__device:
+			self.__object = self.__bus.get_object('com.astro73.saitek.CommandPad', self.__device)
+			self.getMode = self._getMode
+			self.getDevice = self._getDevice
 	
+	def close(self):
+		if hasattr(self.__receiver, 'remove'):
+			self.__receiver.remove()
+		self.__receiver = None
+		self.__up = None
+		self.__down = None
+		self.__mode = None
+		self.__bus = None
+		self.__device = None
+		self.__filter = None
+		self.__object = None
+		
 	def __del__(self):
-		self.__receiver.remove()
+		self.close()
 	
-	def __filter(self, button, mode=None):
+	def _filter(self, button, mode=None):
 		return (self.Filter and button not in (BUTTON_A, BUTTON_B)) or \
 		       (not self.Filter)
 	
@@ -109,44 +242,18 @@ class CommandPadListener(object):
 		member = kw['member']
 		path = kw['path']
 		if member == 'ButtonUp' and callable(self.ButtonUp):
-			if self.__filter(*pargs): self.ButtonUp(path, *pargs)
+			if self._filter(*pargs): self.ButtonUp(path, *pargs)
 		elif member == 'ButtonDown' and callable(self.ButtonDown):
-			if self.__filter(*pargs): self.ButtonDown(path, *pargs)
+			if self._filter(*pargs): self.ButtonDown(path, *pargs)
 		elif member == 'ModeChange' and callable(self.ModeChange):
 			self.ModeChange(path, *pargs)
 	
-	@classmethod
-	def startMainLoop(cls):
-		"""
-		Runs the DBus main loop in a new thread.
-		"""
-		cls.__thread = threading.Thread(target=cls.MainLoop.run, name="dbus-mainloop")
-		cls.__thread.start()
+	def _getDevice(self):
+		return self.__object.getDevice()
 	
-	@classmethod
-	def joinMainLoop(cls, *p, **kw):
-		"""
-		Calls the join() method on the main loop thread created by startMainLoop()
-		"""
-		cls.__thread.join(*p, **kw)
+	def _getMode(self):
+		return self.__object.getMode()
 	
-	@classmethod
-	def quitMainLoop(cls):
-		"""
-		Tells the main loop thread started by startMainLoop() to stop.
-		"""
-		cls.MainLoop.quit()
-	
-	@classmethod
-	def runMainLoop(cls):
-		"""
-		Just runs the main loop instead of threading it.
-		"""
-		try:
-			cls.MainLoop.run()
-		except KeyboardInterrupt:
-			pass
-
 if __name__ == '__main__':
 	import sys
 	def up(path, button, mode):
@@ -164,5 +271,5 @@ if __name__ == '__main__':
 	print "Instantiate"
 	cpl = CommandPadListener(up, down, mode)
 	print "Run"
-	CommandPadListener.runMainLoop()
+	runMainLoop()
 
