7e8e9001f5c1266f10f279db4ee92605ab1d79b2
James Bliss <james.bliss@astro73.com>
2008-02-28 20:27:20 +0000

    Removing kbled, linuxkd (being moved to utils)
    git-svn-id: file:///home/astronouth7303/www/astronouth7303/public_html/astro73.com/svnroot/commandpad/trunk@36 536712d7-3b65-4200-8bf4-cfdaa7821495

--- cmdpadd-udev/setup.py
+++ cmdpadd-udev/setup.py
@@ -12,11 +12,7 @@ joystick = Extension('joystick',
                     swig_opts=swigops,
                     )
 
-linuxkd = Extension('linuxkd',
-                    sources = ['linuxkd.c'],
-                    )
-
 setup (name = 'uinput',
        version = '1.0',
        description = 'some constants',
-       ext_modules = [uinput, joystick, linuxkd])
+       ext_modules = [uinput, joystick])
